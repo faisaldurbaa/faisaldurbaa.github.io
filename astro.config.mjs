3292c1f817c69bbfc03a73f134c3684ddbe43b24
myagnetgit <mygeminia25@gmail.com>
2025-07-20 21:03:11 +0300

    UI improvements and backend updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import rss from "@astrojs/rss";
+import icon from "astro-icon";
 
 export default defineConfig({
   site: 'https://myagnetgit.github.io',
   integrations: [
     mdx(), 
     tailwind(), 
-    sitemap(), 
+    sitemap(),
+    icon() 
   ],
 });
