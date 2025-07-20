37eaf12ab71fc5f6e2803bf7331264638f7eca9f
myagnetgit <mygeminia25@gmail.com>
2025-07-20 14:51:12 +0300

    Completed module-2 of dev plan

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,12 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import rss from "@astrojs/rss";
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://myagnetgit.github.io/', 
   integrations: [
     tailwind(), 
     sitemap(),
-    rss({
-      title: 'Joe Snow | Blog',
-      description: 'A collection of articles on technology, data, and design.',
-      dest: '/rss.xml',
-      customData: `<language>en-us</language>`,
-    })
   ],
 });
