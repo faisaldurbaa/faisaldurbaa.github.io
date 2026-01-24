3a94c74b7b8c2a0a3fd68c3cb73966b6245a50ba
faisaldurbaa <faisaldurbaa07@gmail.com>
2026-01-24 16:52:33 +0300

    minor content updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import rehypeExternalLinks from "rehype-external-links";
 
 export default defineConfig({
   site: 'https://faisaldurbaa.com',
+  markdown: {
+    rehypePlugins: [[rehypeExternalLinks, { target: "_blank", rel: ["noopener", "noreferrer"] }]],
+  },
   integrations: [
     mdx(), 
     tailwind(), 
